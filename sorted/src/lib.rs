2eb31915b37566da04f39f4be37f24a3a7114f95
Daniel Silverstone <dsilvers@digital-scurf.org>
2019-06-21 11:43:15 +0100

    First two bits of sorted

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -2,10 +2,27 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 
+use quote::ToTokens;
+use syn::{parse_macro_input, Error, Item};
+use proc_macro2::Span;
+
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
     let _ = args;
-    let _ = input;
+    let input: Item = parse_macro_input!(input as Item);
 
-    unimplemented!()
+    (match sorted_impl(input) {
+        Ok(t) => t.into_token_stream(),
+        Err(e) => e.to_compile_error(),
+    })
+    .into()
+}
+
+fn sorted_impl(input: Item) -> Result<impl ToTokens, Error> {
+    let ienum = match input {
+        Item::Enum(ienum) => ienum,
+        _ => Err(Error::new(Span::call_site(), "expected enum or match expression"))?,
+    };
+
+    Ok(ienum.into_token_stream())
 }

0948121bf27f2590efe406df2a27b88c7f918c4a
Daniel Silverstone <dsilvers@digital-scurf.org>
2019-06-21 11:52:58 +0100

    Support reporting which variant to report before

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -2,9 +2,10 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 
+use proc_macro2::Span;
 use quote::ToTokens;
 use syn::{parse_macro_input, Error, Item};
-use proc_macro2::Span;
+
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -21,8 +22,34 @@ pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
 fn sorted_impl(input: Item) -> Result<impl ToTokens, Error> {
     let ienum = match input {
         Item::Enum(ienum) => ienum,
-        _ => Err(Error::new(Span::call_site(), "expected enum or match expression"))?,
+        _ => Err(Error::new(
+            Span::call_site(),
+            "expected enum or match expression",
+        ))?,
     };
+
+    let variants: Vec<_> = ienum.variants.iter().map(|v| &v.ident).collect();
+
+    for second in 1..variants.len() {
+        if variants[second] < variants[second - 1] {
+            // variants[second] is out of order, find the earliest spot it ought
+            // to go and report that
+            let mut first = second - 1;
+            while first > 1 {
+                if variants[second] < variants[first - 1] {
+                    first -= 1;
+                }
+            }
+            // Report the error at first
+            Err(Error::new(
+                variants[second].span(),
+                format!(
+                    "{} should sort before {}",
+                    variants[second], variants[first]
+                ),
+            ))?;
+        }
+    }
 
     Ok(ienum.into_token_stream())
 }

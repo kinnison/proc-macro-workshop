3eccc89e2c382c3c2e187b2762ba72634dcbfd57
Daniel Silverstone <dsilvers@digital-scurf.org>
2019-06-21 20:29:11 +0100

    Support match expressions in sorted

--- sorted/src/lib.rs
+++ sorted/src/lib.rs
@@ -3,9 +3,13 @@ extern crate proc_macro;
 use proc_macro::TokenStream;
 
 use proc_macro2::Span;
+use proc_macro2::TokenStream as TS;
 use quote::{quote, ToTokens};
-use syn::{parse_macro_input, Error, Item};
-
+use syn::{
+    parse_macro_input,
+    visit_mut::{visit_expr_match_mut, visit_item_fn_mut, VisitMut},
+    Error, ExprMatch, Item, ItemFn, Pat, Path,
+};
 
 #[proc_macro_attribute]
 pub fn sorted(args: TokenStream, input: TokenStream) -> TokenStream {
@@ -58,4 +62,84 @@ fn sorted_impl(input: &Item) -> Result<impl ToTokens, Error> {
     }
 
     Ok(ienum.into_token_stream())
+}
+
+struct SortedVisitor {
+    errs: Vec<Error>,
+}
+
+impl SortedVisitor {
+    pub fn new() -> SortedVisitor {
+        SortedVisitor { errs: Vec::new() }
+    }
+
+    pub fn errors(&self) -> TS {
+        let errs: Vec<_> = self.errs.iter().map(|e| e.to_compile_error()).collect();
+        quote! {
+            #(#errs)*
+        }
+    }
+}
+
+fn name_from_pattern(pat: &Pat) -> Option<&Path> {
+    match pat {
+        Pat::Struct(s) => Some(&s.path),
+        Pat::TupleStruct(ts) => Some(&ts.path),
+        Pat::Path(p) => Some(&p.path),
+        _ => None,
+    }
+}
+
+impl VisitMut for SortedVisitor {
+    fn visit_expr_match_mut(&mut self, expr: &mut ExprMatch) {
+        let is_sorted = expr.attrs.iter().any(|att| att.path.is_ident("sorted"));
+        if is_sorted {
+            // We need to remove the sorted attribute
+            expr.attrs
+                .retain(|att| att.path.is_ident("sorted") == false);
+            // Now we need to verify that each arm is in order...
+            let all_pats: Vec<_> = expr
+                .arms
+                .iter()
+                .flat_map(|arm| arm.pats.iter().flat_map(name_from_pattern))
+                .collect();
+
+            let pat_names: Vec<_> = all_pats
+                .iter()
+                .map(|pat| pat.into_token_stream().to_string())
+                .collect();
+
+            for second in 1..all_pats.len() {
+                for first in 0..second {
+                    if pat_names[second] < pat_names[first] {
+                        // Second should sort before first
+                        let err = Error::new_spanned(
+                            all_pats[second],
+                            format!(
+                                "{} should sort before {}",
+                                pat_names[second], pat_names[first]
+                            ),
+                        );
+                        self.errs.push(err);
+                    }
+                }
+            }
+        }
+        visit_expr_match_mut(self, expr);
+    }
+}
+
+#[proc_macro_attribute]
+pub fn check(_attrs: TokenStream, input: TokenStream) -> TokenStream {
+    let mut input = parse_macro_input!(input as ItemFn);
+    let mut visitor = SortedVisitor::new();
+
+    visit_item_fn_mut(&mut visitor, &mut input);
+    let errors = visitor.errors();
+
+    (quote! {
+        #input
+        #errors
+    })
+    .into()
 }
